38a1e3e665bc24e3dbf5dce64c554b5edda9be68
hjh034850 <hujihuimo@163.com>
2017-12-10 21:13:28 +0800

    jquery遮罩层

--- 遮罩层/dialog.js
+++ 遮罩层/dialog.js
@@ -9,6 +9,7 @@ requirejs.config({
 define(['jquery'],function($){
     return {
         open:function(arg){
+            var _this = this;
             this.defaultValue = {
                 width:300,
                 height:200
@@ -23,25 +24,29 @@ define(['jquery'],function($){
                 +'</div>'
                 +'</div>';*/
             var value = $.extend(this.defaultValue,arg);
-            var $mask = $( '<div class="mask"></div>');
-            var $container = $('<div class="container"></div>');
-            var $title = $('<div class="title"></div>');
-            var $H2 = $('<h2></h2>');
-            var $close = $('<span class="close"></span>');
-            var $content = $('<div class="content"></div>');
-            $(".container").appendTo($mask).append($title).append($content).css({
+            this.$mask = $( '<div class="mask"></div>');
+            this.$container = $('<div class="container"></div>');
+            this.$title = $('<div class="title"></div>');
+            this.$H2 = $('<h2></h2>').html(value.title);
+            this.$close = $('<span class="close">×</span>').on("click",function(){
+                _this.close();
+            });
+            this.$content = $('<div class="content"></div>');
+            this.$container.appendTo(this.$mask).append(this.$title).append(this.$content).css({
                 width:value.width,
                 height:value.height,
                 marginLeft:-value.width/2,
                 marginTop:-value.height/2
             });
-            $title.append($H2).append($close).html(value.title);
-            $content.load(value.content);
-            $("body").append($mask);
-
+            this.$title.append(this.$H2).append(this.$close);
+            this.$content.load(value.content);
+            $("body").append(this.$mask);
 
             /*$(".title h2").html(arg.title);
             $(".content").html(arg.content)*/
+        },
+        close:function(){
+            this.$mask.remove();
         }
     }
 })
