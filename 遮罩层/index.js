62dcef8405192eacc0096faa14e6d427d68ff63f
hjh034850 <hujihuimo@163.com>
2017-12-10 21:27:59 +0800

    jquery遮罩层

---
+++ 遮罩层/index.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Administrator on 2017/12/10.
+ */
+requirejs.config({
+    paths: {
+        jquery: '../jquery-1.12.4'
+    }
+});
+require(['jquery','dialog'],function($,dialog){
+    $("#btn").on("click",function(){
+        dialog.open({
+            width:800,
+            title:"niaho",
+            content:"pianduan.html"
+        });
+    })
+    $("body").on("click","#btn",function(){
+        dialog.close();
+    })
+});
