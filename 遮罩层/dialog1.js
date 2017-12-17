32e785fd5a391bc91fbca94324c41122248c27f6
hjh034850 <hujihuimo@163.com>
2017-12-17 22:56:02 +0800

    照片墙

--- 遮罩层/dialog1.js
+++ 遮罩层/dialog1.js
@@ -14,13 +14,13 @@ define(['jquery'],function($){
             height: 200
         }
         $.extend(this.defaultValue,arg);
-        
         this.$mask = $( '<div class="mask"></div>');
         this.$container = $('<div class="container"></div>');
         this.$title = $('<div class="title"></div>');
         this.$H2 = $('<h2></h2>').html(this.defaultValue.title);
         this.$close = $('<span class="close">×</span>').on("click",function(){
             _this.close();
+
         });
         this.$content = $('<div class="content"></div>');
     }
